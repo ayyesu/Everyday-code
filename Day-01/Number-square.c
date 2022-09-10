e4a2d6731221183b78cb34731beeb83ca5ae4f34
Daniel Kissiedu <iamdanielkissiedu@gmail.com>
2022-09-10 21:03:04 +0000

    Added Day1 file

---
+++ Day-01/Number-square.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+// Finding the square of numbers from 0-10
+int a;
+int i;
+for (i = 0; i<=10; i++) 
+{
+    a = i*i;
+    printf("%d\n", a);
+}
+ 
+ return(0);
+}
---
+++ Day-01/README.md
@@ -0,0 +1,1 @@
+Finding the square of numbers from 0-10
